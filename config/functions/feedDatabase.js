827940e247cafeb04940ec17e7e91417da7e0f75
El asli Karim <k.elasli@icloud.com>
2020-06-08 17:54:50 +0200

    feat(api:recipes): create recipes management

--- config/functions/feedDatabase.js
+++ config/functions/feedDatabase.js
@@ -1,7 +1,7 @@
 const foodsCategories = require('../feed/foods_categories.json');
 
 module.exports = async () => {
-  const categories = await strapi.services['foods-categories'].find();
+  let categories = await strapi.services['foods-categories'].find();
   if (categories.length === 0) {
     for (const foodsCategory of foodsCategories) {
       await strapi.services['foods-categories'].create({
